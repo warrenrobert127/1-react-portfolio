9edec8a0b12cd91d84085aa6b18551fe3f407cfa
Robert Warren <warrenrobert127@gmail.com>
2022-03-01 22:23:44 -0600

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "portfolio",
+      description: "Some of my best web development work",
+    },
+    { name: "resume", description: "My work and education experience" },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ my-app/src/components/Footer/Index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="my-3">
+        <a href="https://github.com/warrenrobert127/1-react-portfolio">
+          Github
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
